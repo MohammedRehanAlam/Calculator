e547efeb461b2f09b0f60d39fd322b981f97ef87
MohammedRehanAlam <mohammedrehanalam16@gmail.com>
2025-09-06 15:38:06 +0530

    added the new changes to make the html file to apk and aab

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.yourcompany.calculator',
   appName: 'Calculator',
-  webDir: 'docs', // dist 
+  webDir: 'docs',
 
   server: {
     androidScheme: 'https'
